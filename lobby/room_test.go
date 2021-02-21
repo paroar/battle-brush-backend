1e2a4b4fdd1f2371836d5d7571d671e1932e6ba4
paroar <prodriguezarmida@gmail.com>
2021-02-21 18:51:22 +0100

    Add room tests

---
+++ lobby/room_test.go
@@ -0,0 +1,57 @@
+package lobby
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/paroar/battle-brush-backend/generators"
+)
+
+func TestRoomLeaveClient(t *testing.T) {
+	l := NewLobby()
+	go l.Run()
+
+	client := &Client{
+		name:  generators.Name(),
+		id:    uuid.NewString(),
+		lobby: l,
+		send:  make(chan *Message),
+	}
+
+	l.joinClient(client)
+
+	room := l.CreatePrivateRoom(client)
+
+	room.leaveClient(client)
+
+	_, err := room.getClient(client.id)
+	if err == nil {
+		t.Fatal("room.getClient should return an error")
+	}
+
+}
+
+func TestRoomGetClient(t *testing.T) {
+	l := NewLobby()
+	go l.Run()
+
+	client := &Client{
+		name:  generators.Name(),
+		id:    uuid.NewString(),
+		lobby: l,
+		send:  make(chan *Message),
+	}
+
+	l.joinClient(client)
+
+	room := l.CreatePrivateRoom(client)
+
+	c, err := room.getClient(client.id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if c.id != client.id {
+		t.Fatal("room.getClient should return the same client")
+	}
+}
