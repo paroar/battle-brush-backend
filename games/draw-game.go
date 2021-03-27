92f53ec8f92a025c00ba593ca4b9a548e1b3ab29
paroar <prodriguezarmida@gmail.com>
2021-03-27 19:24:02 +0100

    Magic numbers

--- games/draw-game.go
+++ games/draw-game.go
@@ -24,6 +24,14 @@ type score struct {
 	AvgScore float64
 }
 
+const (
+	LOADING_TIME     = 5
+	DRAWING_TIME     = 45
+	WINNER_TIME      = 10
+	VOTING_TIME      = 10
+	RECOLECTING_TIME = 1
+)
+
 // NewDrawGame constructor
 func NewDrawGame(id string, players []string) *DrawGame {
 	return &DrawGame{
@@ -43,27 +51,27 @@ func (d *DrawGame) StartGame(l *websocket.Lobby) {
 	}
 
 	//Drawing
-	d.changeState(room, l, StateLoadingDrawing, 5)
-	d.changeState(room, l, StateDrawing, 60)
+	d.changeState(room, l, model.StateLoadingDrawing, LOADING_TIME)
+	d.changeState(room, l, model.StateDrawing, DRAWING_TIME)
 
 	//Recolecting Img
-	d.changeState(room, l, StateRecolecting, 1)
+	d.changeState(room, l, model.StateRecolecting, RECOLECTING_TIME)
 
 	//Voting
-	d.changeState(room, l, StateLoadingVoting, 5)
+	d.changeState(room, l, model.StateLoadingVoting, LOADING_TIME)
 	d.voting(room, l)
 
 	//Winner
 	d.win(room, l)
-	d.changeState(room, l, StateLoadingWinner, 5)
-	d.changeState(room, l, StateWinner, 10)
+	d.changeState(room, l, model.StateLoadingWinner, LOADING_TIME)
+	d.changeState(room, l, model.StateWinner, WINNER_TIME)
 
 	//Clean db
 	d.cleaning()
 
 	//Waiting
-	d.changeState(room, l, StateLoading, 1)
-	d.changeState(room, l, StateWaiting, 1)
+	d.changeState(room, l, model.StateLoading, LOADING_TIME)
+	d.changeState(room, l, model.StateWaiting, LOADING_TIME)
 }
 
 func (d *DrawGame) broadcastState(l *websocket.Lobby, state string) {
@@ -141,8 +149,8 @@ func (d *DrawGame) voting(room *model.Room, l *websocket.Lobby) {
 		msg := content.NewImage(img.Img, img.PlayerID)
 		l.Broadcast(d.Players, msg)
 
-		d.changeState(room, l, StateVoting, 10)
-		d.changeState(room, l, StateRecolectingVotes, 1)
-		d.changeState(room, l, StateLoading, 1)
+		d.changeState(room, l, model.StateVoting, VOTING_TIME)
+		d.changeState(room, l, model.StateRecolectingVotes, RECOLECTING_TIME)
+		d.changeState(room, l, model.StateLoading, RECOLECTING_TIME)
 	}
 }
