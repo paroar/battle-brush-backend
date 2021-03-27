ca3dfeb0e7e215ef965895fccf782c112a16e35e
paroar <prodriguezarmida@gmail.com>
2021-03-27 19:18:17 +0100

    Moved GameStates

--- games/i_game.go
+++ games/i_game.go
@@ -4,18 +4,3 @@ package games
 type IGame interface {
 	StartGame()
 }
-
-// Common Game states
-const (
-	StateDrawing          = "Drawing"
-	StateLoadingDrawing   = "LoadingDrawing"
-	StateVoting           = "Voting"
-	StateLoadingVoting    = "LoadingVoting"
-	StateRecolecting      = "Recolecting"
-	StateStart            = "Start"
-	StateWaiting          = "Waiting"
-	StateLoading          = "Loading"
-	StateRecolectingVotes = "RecolectingVotes"
-	StateLoadingWinner    = "LoadingWinner"
-	StateWinner           = "Winner"
-)
---
+++ model/gameStates.go
@@ -0,0 +1,16 @@
+package model
+
+// Common Game states
+const (
+	StateDrawing          = "Drawing"
+	StateLoadingDrawing   = "LoadingDrawing"
+	StateVoting           = "Voting"
+	StateLoadingVoting    = "LoadingVoting"
+	StateRecolecting      = "Recolecting"
+	StateStart            = "Start"
+	StateWaiting          = "Waiting"
+	StateLoading          = "Loading"
+	StateRecolectingVotes = "RecolectingVotes"
+	StateLoadingWinner    = "LoadingWinner"
+	StateWinner           = "Winner"
+)
